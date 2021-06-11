b29c5f1e6b884bb1395883731416a0ce7d4a7096
dojo-2021-s-yagi <yagi@seplus2016.onmicrosoft.com>
2021-06-11 16:12:03 +0900

    1

---
+++ Cpull/src/dao/InquiryDAO.java
@@ -0,0 +1,63 @@
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InquiryDAO {
+	// 引数inquirycardで指定されたレコードを登録し、成功したらtrueを返す
+	public boolean insert(Inquiry inquirycard) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する //後で参照ファイルを指定
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "sa");
+
+			// SQL文を準備する
+			String sql = "insert into BC values(null, null, ?, ?, ?,)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			if (inquirycard.getSubject() != null) {
+				pStmt.setString(1, inquirycard.getSubject());
+			} else {
+				pStmt.setString(1, "null");
+			}
+			if (inquirycard.getMessage() != null) {
+				pStmt.setString(2, inquirycard.getMessage());
+			} else {
+				pStmt.setString(2, "null");
+			}
+			if (inquirycard.getRegistdate() != null) {
+				pStmt.setTimestamp(3, inquirycard.getRegistdate());
+			} else {
+				pStmt.setTimestamp(3, null);
+			}
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
+
+
+}
--- Cpull/src/servlet/InquiryServlet.java
+++ Cpull/src/servlet/InquiryServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.InquiryDAO;
 
 /**
  * Servlet implementation class InquiryServlet
@@ -17,17 +22,42 @@ public class InquiryServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/Cpull/LoginServlet");
+			return;
+		}
+
+		// お問い合わせページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/inquiry.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/Cpull/LoginServlet");
+			return;
+		}
 
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String subject= request.getParameter("SUBJECT");
+		String message= request.getParameter("MESSAGE");
+		// 登録処理を行う
+		InquiryDAO iDao = new InquiryDAO();
+		iDao.insert(new inquiry(0, 0, subject, message, null)); //お問い合わせID,ユーザーID,件名,内容,お問い合わせ時刻
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/i_result.jsp");
+		dispatcher.forward(request, response);
+	}
 }
