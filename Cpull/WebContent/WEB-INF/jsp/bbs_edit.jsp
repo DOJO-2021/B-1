d1f24bdef9a105aaf2bb63a8ddcb00fa64b4dd51
dojo2021-mine <mine@seplus2016.onmicrosoft.com>
2021-06-14 15:16:17 +0900

    mine

--- Cpull/WebContent/WEB-INF/jsp/bbs_edit.jsp
+++ Cpull/WebContent/WEB-INF/jsp/bbs_edit.jsp
@@ -4,9 +4,53 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>スレッド編集</title>
 </head>
 <body>
+<h1>スレッド編集</h1>
+<form action="">
+タイトル<br>
+	<input type="text" name="title" value="${e.title}">
+内容<br>
+	<input type="text" name="detail" value="${e.detail}">
+<div>
+	<label for="kind">悩み</label>
+	<select id="kind" name="kind">
+		<option value="personal">パーソナル</option>
+		<option value="it_basic">IT基礎</option>
+		<option value="java_basic">Java基礎</option>
+		<option value="development">開発演習</option>
+		<option value="other">その他</option>
+	</select>
+</div>
 
+<div>
+	<label for="kind">質問</label>
+	<select id="kind" name="kind">
+		<option value="personal">パーソナル</option>
+		<option value="it_basic">IT基礎</option>
+		<option value="everyone">Java基礎</option>
+		<option value="development">開発演習</option>
+		<option value="other">その他</option>
+	</select>
+</div>
+
+<div>
+	<label for="kind">公開範囲</label>
+	<select id="kind" name="kind">
+		<option value="everyone">すべての人</option>
+		<option value="student">受講者のみ</option>
+	</select>
+</div>
+<div>
+	パスワード<br>
+	<input type="text" name="pw">
+</div>
+<div>
+	<input type="submit" name="submit" value="下書き保存">
+	<input type="submit" name="submit" value="更新">
+	<input type="submit" name="submit" value="削除">
+</div>
+</form>
 </body>
 </html>
---
+++ Cpull/src/model/Reply.java
@@ -0,0 +1,69 @@
+package model;
+import java.io.Serializable;
+
+public class Reply implements Serializable {
+	private int bbs_id;
+	private String user_id;
+	private String user_name;
+	private String reply_name;
+	private int reply_range;
+	private String reply_contents;
+
+	//コンストラクタ引数あり
+public Reply(int bbs_id,String user_id, String user_name, String  reply_name,int reply_range, String reply_contents) {
+	super();
+	this.bbs_id = bbs_id;
+	this.user_id = user_id;
+	this.user_name = user_name;
+	this.reply_name = reply_name;
+	this.reply_range = reply_range;
+	this.reply_contents =reply_contents;
+}
+//コンストラクタ引数なし
+public Reply() {
+	super();
+	this.bbs_id = 0;
+	this.user_id = "";
+	this.user_name = "";
+	this.reply_name = "";
+	this.reply_range = 0;
+	this.reply_contents ="";
+}
+public int getBbs_id() {
+	return bbs_id;
+}
+public void setBbs_id(int bbs_id) {
+	this.bbs_id = bbs_id;
+}
+public String getUser_id() {
+	return user_id;
+}
+public void setUser_id(String user_id) {
+	this.user_id = user_id;
+}
+public String getUser_name() {
+	return user_name;
+}
+public void setUser_name(String user_name) {
+	this.user_name = user_name;
+}
+public String getReply_name() {
+	return reply_name;
+}
+public void setReply_name(String reply_name) {
+	this.reply_name = reply_name;
+}
+public int getReply_range() {
+	return reply_range;
+}
+public void setReply_range(int reply_range) {
+	this.reply_range = reply_range;
+}
+public String getReply_contents() {
+	return reply_contents;
+}
+public void setReply_contents(String reply_contents) {
+	this.reply_contents = reply_contents;
+}
+
+}
