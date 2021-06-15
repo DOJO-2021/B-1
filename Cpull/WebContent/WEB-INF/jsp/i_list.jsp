28e2cad8b0d05d079bd6ed849d0e029c76df4b40
dojo-2021-s-yagi <yagi@seplus2016.onmicrosoft.com>
2021-06-15 16:15:09 +0900

    1

--- Cpull/WebContent/WEB-INF/jsp/i_list.jsp
+++ Cpull/WebContent/WEB-INF/jsp/i_list.jsp
@@ -10,13 +10,27 @@
 	<header> </header>
 	<main>
 		<p>お問い合わせ入力内容</p>
-			<form method="GET" action="/Cpull/IuquiryServlet">
-			<!-- ID<input type="text" name="USER_ID" value="${Inquiry.user_id}"><br> -->
-			お問い合わせID<input type="text" name="Inquiry_id" value="3"><br>
-			件名<input type="text" name="SUBJECT" value="トークルームについて"><br>
-			内容<input type="text" name="MESSAGE" value="雑談部屋をもう１部屋追加してほしいです"><br>
-			お問い合わせ時刻<input type="text" name="TS" value="2021-06-15 11:49:03.339862"><br>
-			</form>
+		<!-- ID<input type="text" name="USER_ID" value="${Inquiry.user_id}"><br> -->
+		<table>
+			<tr>
+				<th>お問い合わせID</th>
+				<th><input type="text" name="Inquiry_id" value="3"></th>
+			</tr>
+			<tr>
+				<th>件名</th>
+				<th><input type="text" name="SUBJECT" value="トークルームについて"></th>
+			</tr>
+			<tr>
+				<th>内容</th>
+				<th><input type="text" name="MESSAGE"
+					value="雑談部屋をもう１部屋追加してほしいです"></th>
+			</tr>
+			<tr>
+				<th>お問い合わせ時刻</th>
+				<th><input type="text" name="TS"
+					value="2021-06-15 11:49:03.339862"></th>
+			</tr>
+		</table>
 	</main>
 </body>
 </html>
--- Cpull/src/servlet/InquiryListServlet.java
+++ Cpull/src/servlet/InquiryListServlet.java
@@ -1,14 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
- * Servlet implementation class InquiryListServlet
+ * Servlet implementation class InquiryServlet
  */
 @WebServlet("/InquiryListServlet")
 public class InquiryListServlet extends HttpServlet {
@@ -17,11 +20,20 @@ public class InquiryListServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		//ログインの部分は後でコメントアウトを外す
+		//		HttpSession session = request.getSession();
+		//		if (session.getAttribute("id") == null) {
+		//			response.sendRedirect("/Cpull/LoginServlet");
+		//			return;
+		//		}
 
+		// お問い合わせの内容リストにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/i_list.jsp");
+		dispatcher.forward(request, response);
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
