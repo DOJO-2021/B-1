b3fff4a89b01758d15e218ac3d51773c38a450e6
dojo2021-k-moriyama <moriyama@seplus2016.onmicrosoft.com>
2021-06-22 15:16:52 +0900

    moriyama 15:16

--- Cpull/src/servlet/BbsServlet.java
+++ Cpull/src/servlet/BbsServlet.java
@@ -9,9 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.BbsDAO;
 import model.BBS;
+import model.BBSt;
 
 /**
  * Servlet implementation class BbsServlet
@@ -43,6 +45,12 @@ public class BbsServlet extends HttpServlet {
 
 		// 検索結果をリクエストスコープに格納する
 		request.setAttribute("bbsList", bbsList);
+
+		//検索ワードをセッションスコープに格納する
+		BBSt t = new BBSt();
+		t.setBbs_title(bbs_title);
+		HttpSession session = request.getSession();
+		session.setAttribute("t",t);
 
 		// 結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/bbs_searchresult.jsp");
