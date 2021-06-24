9faf2471d6741aacb6c1c94d1cdd2e2dd3248dcf
dojo-2021-s-yagi <yagi@seplus2016.onmicrosoft.com>
2021-06-24 11:25:01 +0900

    ya

--- Cpull/WebContent/js/bbs_create.js
+++ Cpull/WebContent/js/bbs_create.js
@@ -1,3 +1,32 @@
-/**
- * 
- */
+'use strict';
+
+//作成ボタンが押された時に確認のダイアログ
+function /* onSubmit="return 〇〇()"の〇〇 */ () {
+	//確認ダイアログを表示
+	if(window.confirm('この内容で作成してよろしいですか？')){
+		//「OK」時は送信実行
+		return true;
+	}
+	//「キャンセル」時
+	else {
+		window.alert('キャンセルされました。');
+		//送信中止
+		return false;
+	}
+}
+
+
+//下書きボタンが押された時に確認のダイアログ
+function /* onSubmit="return 〇〇()"の〇〇 */ () {
+	//確認ダイアログを表示
+	if(window.confirm('この内容で保存してよろしいですか？')){
+		//「OK」時は送信実行
+		return true;
+	}
+	//「キャンセル」時
+	else {
+		window.alert('キャンセルされました。');
+		//送信中止
+		return false;
+	}
+}
