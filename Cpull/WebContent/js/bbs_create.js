4a25e15ca781825f108d08fc05aef4c7873ef633
dojo-2021-s-yagi <yagi@seplus2016.onmicrosoft.com>
2021-06-24 15:28:55 +0900

    ya

--- Cpull/WebContent/js/bbs_create.js
+++ Cpull/WebContent/js/bbs_create.js
@@ -1,7 +1,8 @@
 'use strict';
 
 //作成ボタンが押された時に確認のダイアログ
-function /* onSubmit="return 〇〇()"の〇〇 */ () {
+document.getElementById("push").onclick =
+function push() {
 	//確認ダイアログを表示
 	if(window.confirm('この内容で作成してよろしいですか？')){
 		//「OK」時は送信実行
@@ -17,7 +18,8 @@ function /* onSubmit="return 〇〇()"の〇〇 */ () {
 
 
 //下書きボタンが押された時に確認のダイアログ
-function /* onSubmit="return 〇〇()"の〇〇 */ () {
+document.getElementById("save").onclick =
+function save() {
 	//確認ダイアログを表示
 	if(window.confirm('この内容で保存してよろしいですか？')){
 		//「OK」時は送信実行
@@ -29,4 +31,13 @@ function /* onSubmit="return 〇〇()"の〇〇 */ () {
 		//送信中止
 		return false;
 	}
+}
+
+function ShowHideDiv() {
+	var worry = document.getElementById("worry");
+	var question = document.getElementById("question");
+	var worryPass = document.getElementById("worryPass");
+	var questionPass = document.getElementById("questionPass");
+	worryPass.style.display = worry.checked ? "block" : "none";
+	questionPass.style.display = question.checked ? "block" : "none";
 }
